994c5085353745b35601854a7adb3655f53c89de
Dan Ross <daniellross@gmail.com>
2021-06-15 12:13:23 -0500

    added update method for brewmethods

--- brew_shareapi/views/brew_method.py
+++ brew_shareapi/views/brew_method.py
@@ -18,6 +18,7 @@ class BrewMethodView(ViewSet):
         """
         new_method = BrewMethod()
         new_method.brewer = Brewer.objects.get(user=request.auth.user)
+        new_method.method_image = request.data["methodImage"]
         new_method.website = request.data["website"]
         new_method.name = request.data["name"]
 
@@ -51,6 +52,24 @@ class BrewMethodView(ViewSet):
             method = BrewMethod.objects.get(pk=pk)
             serializer = MethodSerializer(method, context={'request': request})
             return Response(serializer.data)
+        except BrewMethod.DoesNotExist as ex:
+            return Response({'message': ex.args[0]}, status=status/status.HTTP_404_NOT_FOUND)
+        except Exception as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    def update(self, request, pk=None):
+        """Handle PUT request for a method"""
+        brewer = Brewer.objects.get(user=request.auth.user)
+        try:
+            if brewer.is_admin:
+                method = BrewMethod.objects.get(pk=pk)
+            else:
+                method = BrewMethod.objects.get(pk=pk, brewer=brewer)
+            method.method_image = request.data["methodImage"]
+            method.website = request.data["website"]
+            method.name = request.data["name"]
+            method.save()
+            
         except BrewMethod.DoesNotExist as ex:
             return Response({'message': ex.args[0]}, status=status/status.HTTP_404_NOT_FOUND)
         except Exception as ex:
