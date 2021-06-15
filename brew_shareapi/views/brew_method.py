6acaf6d66b44da245ad11953210bcbf5455c71db
Dan Ross <daniellross@gmail.com>
2021-06-15 13:10:16 -0500

    added delete method for brewmethods

--- brew_shareapi/views/brew_method.py
+++ brew_shareapi/views/brew_method.py
@@ -69,7 +69,22 @@ class BrewMethodView(ViewSet):
             method.website = request.data["website"]
             method.name = request.data["name"]
             method.save()
-            
+            return Response({}, status=status.HTTP_204_NO_CONTENT)
+        except BrewMethod.DoesNotExist as ex:
+            return Response({'message': ex.args[0]}, status=status/status.HTTP_404_NOT_FOUND)
+        except Exception as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    def destroy(self, request, pk=None):
+        """Handle DELETE requests for brew methods"""
+        brewer = Brewer.objects.get(user=request.auth.user)
+        try:
+            if brewer.is_admin:
+                method = BrewMethod.objects.get(pk=pk)
+            else:
+                method = BrewMethod.objects.get(pk=pk, brewer=brewer)
+            method.delete()
+            return Response({}, status=status.HTTP_204_NO_CONTENT)
         except BrewMethod.DoesNotExist as ex:
             return Response({'message': ex.args[0]}, status=status/status.HTTP_404_NOT_FOUND)
         except Exception as ex:
