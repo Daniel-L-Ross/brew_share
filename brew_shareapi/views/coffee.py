b0e6cf898e9858930b8d4681233bcc906a408667
Dan Ross <daniellross@gmail.com>
2021-06-18 10:34:15 -0500

    Added try-catch on coffee update for images

--- brew_shareapi/views/coffee.py
+++ brew_shareapi/views/coffee.py
@@ -78,7 +78,6 @@ class CoffeeView(ViewSet):
             else:
                 coffee = Coffee.objects.get(pk=pk, brewer=brewer)
 
-            coffee.coffee_image = request.data["CoffeeImage"]
             coffee.roaster = request.data["roaster"]
             coffee.website = request.data["website"]
             coffee.name = request.data["name"]
@@ -87,6 +86,13 @@ class CoffeeView(ViewSet):
             coffee.process = request.data["process"]
             coffee.recommended_method = request.data["recommendedMethod"]
             coffee.tasting_notes = request.data["tastingNotes"]
+
+            try:
+                image_data = base64_image_handler(request.data["coffeeImage"], coffee.name)
+                coffee.coffee_image = image_data
+            except:
+                coffee.coffee_image = None
+
             coffee.save()
 
             return Response({}, status=status.HTTP_204_NO_CONTENT)
