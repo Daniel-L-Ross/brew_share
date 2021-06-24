93de7a2c18d66c66e508ad67239eb7236aebe4ff
Dan Ross <daniellross@gmail.com>
2021-06-24 11:06:39 -0500

    removed trailing slash

--- brew_share/settings.py
+++ brew_share/settings.py
@@ -67,7 +67,7 @@ REST_FRAMEWORK = {
 CORS_ORIGIN_WHITELIST = (
     'http://localhost:3000',
     'http://127.0.0.1:3000',
-    'https://brew-share.netlify.app/'
+    'https://brew-share.netlify.app'
 )
 
 MIDDLEWARE = [
