82a5bd1499d7289dcddcf8b704d381c21324d8c7
Dan Ross <daniellross@gmail.com>
2021-06-24 18:34:29 -0500

    updated brew_methods model to handle urls instead of image location

--- brew_shareapi/models/brew_method.py
+++ brew_shareapi/models/brew_method.py
@@ -2,8 +2,6 @@ from django.db import models
 
 class BrewMethod(models.Model):
     brewer = models.ForeignKey('Brewer', on_delete=models.DO_NOTHING)
-    method_image = models.ImageField(
-        upload_to='method_pics/%Y/%m/%d', height_field=None,
-        width_field=None, max_length=None, null=True, blank=True)
-    website = models.CharField(max_length=255, blank=True)
+    method_image = models.URLField(blank=True)
+    website = models.URLField(blank=True)
     name = models.CharField(max_length=50)
